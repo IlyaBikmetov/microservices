7183003841ca62eaee2bf6e11212d0de4c813b31
Ilya Bikmetov <ilyab_v@mail.ru>
2023-01-22 16:51:10 +0500

    Initial commit

---
+++ src/test/kotlin/ru/ibikmetov/microservices/MicroservicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.ibikmetov.microservices
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
